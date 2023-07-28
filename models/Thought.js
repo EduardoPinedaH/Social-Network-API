947769afc3775f8be905a253c4214654b1fbcd94
EduardoPinedaH <eduardopinedahu@gmail.com>
2023-07-28 14:48:12 -0600

    thougt model finished

--- models/Thought.js
+++ models/Thought.js
@@ -38,5 +38,31 @@ const thoughtSchema = new Schema(
             required: true,
             maxlength: 280,
         },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => dateFormat(createdAtVal),
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
     }
-)
+);
+
+// Virtual called `reactionCount` that retrieves the length of the thought's `reactions` array field on query.
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
