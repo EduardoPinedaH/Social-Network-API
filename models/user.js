8f803ce9166463ac9037e9c4a4c1b437b37b5a39
EduardoPinedaH <eduardopinedahu@gmail.com>
2023-07-28 10:32:39 -0600

    user model finished and thought model created

---
+++ models/user.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, "Your email was Wrong,please enter a valid email address",]
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        },
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        },
+    ]
+}, {
+    toJSON: {
+        virtuals: true,
+        getter: true
+    },
+    id: false
+})
