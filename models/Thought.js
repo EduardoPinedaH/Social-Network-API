d00787c108c262c49875232ed51ba1cd6fa1cdee
EduardoPinedaH <eduardopinedahu@gmail.com>
2023-07-28 13:52:50 -0600

    thought schema model updated

--- models/Thought.js
+++ models/Thought.js
@@ -1,12 +1,42 @@
 const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
+// Schema for the reaction
 const reactionSchema = new Schema(
     {
         reactionId: {
             type: Schema.Types.ObjectId,
             default: () => new Types.ObjectId(),
         },
-        
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => dateFormat(createdAtVal),
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+    }
+);
+
+// Schema for thoughts
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            maxlength: 280,
+        },
     }
 )
