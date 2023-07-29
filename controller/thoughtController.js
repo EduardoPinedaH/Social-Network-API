7a06575e5b483397251bdb880238ab4ad97c448c
EduardoPinedaH <eduardopinedahu@gmail.com>
2023-07-28 19:15:12 -0600

    thought controller finished

--- controller/thoughtController.js
+++ controller/thoughtController.js
@@ -63,9 +63,27 @@ const thoughtController = {
         .catch((err) => res.status(500).json(err));
     },
 
-    // Add Reaction
+    // Add reaction
     addReaction(req, res) {
         console.log('Adding reaction');
-        console.log(req.body)
-    }
+        console.log(req.body);
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $addToSet: { reactions: req.body} },
+            { runValidators: true, new: true },
+        ).then((thought) => !thought ? res.status(404).jsn({ message: 'No friend found' }) : res.json(thought))
+        .catch((err) => res.status(500).json(err));
+    },
+    // Delete reaction
+    deleteReaction(req, res) {
+        console.log(req.params)
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $pull: { reactions: { reactionId: req.params.reactionId} } },
+            { runValidators: true, new: true },
+        ).then((thought) => !thought ? res.status(404).json({ message: 'No thought found' }) : res.json(thought))
+        .catch((err) => res.status(500).json(err));
+    },
 }
+
+module.exports = thoughtController;
